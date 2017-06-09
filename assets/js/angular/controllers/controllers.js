45923e2aac94a7fe3bdebc4fa64a55a72bb7fd82
peo_gcruz <cad_gcruz@uolinc.com>
2017-06-08 21:37:23 -0300

    a

--- assets/js/angular/controllers/controllers.js
+++ assets/js/angular/controllers/controllers.js
@@ -137,88 +137,20 @@ app.controller('HomeController', function($rootScope,$scope,User,Score,Home,$tim
 
     $timeout(function(){
 
-    	var data = Statistics.getData();
-        console.dir(data);
-
         $scope.dados = {};
-        
-        // Herois
-        $scope.dados.herois = [];
-        for (var a in data.herois) {
-            
-            data.herois[a].hero = a;
-            
-            var win = parseInt(data.herois[a].win);
-            var draw = parseInt(data.herois[a].draw);
-            var loss = parseInt(data.herois[a].loss);
 
-            var total = win + draw + loss;
+        var herois = Statistics.herois.getData();
+        var mapas = Statistics.mapas.getData();
+        var tipos = Statistics.tipos.getData();
 
-            var winPercentage = (win / total) * 100;
-            var lossPercentage = (loss / total) * 100;
+        var porMapa = Statistics.heroisPorMapa.getData();
+        console.dir(Statistics.tipos.getData());
 
-            data.herois[a].total = total;
-            data.herois[a].winPercentage = winPercentage;
-            data.herois[a].lossPercentage = lossPercentage;
-            $scope.dados.herois.push(data.herois[a]);
-        }
+        $scope.dados.herois = herois;
+        $scope.dados.mapas = mapas;
+        $scope.dados.tipos = tipos;
 
-        // Mapas
-        $scope.dados.mapas = [];
-        for (var a in data.mapas) {
-                       
-            var win = parseInt(data.mapas[a].win);
-            var draw = parseInt(data.mapas[a].draw);
-            var loss = parseInt(data.mapas[a].loss);
-
-            var total = win + draw + loss;
-
-            var winPercentage = (win / total) * 100;
-            var lossPercentage = (loss / total) * 100;
-
-            data.mapas[a].total = total;
-            data.mapas[a].winPercentage = winPercentage;
-            data.mapas[a].lossPercentage = lossPercentage;
-            $scope.dados.mapas.push(data.mapas[a]);
-        }
-
-        // Score
-        $scope.dados.scores = [];
-        for (var a in data.scores) {
-            var b = {};
-            b[a] = data.scores[a].scores;
-            $scope.dados.scores.push(b);
-            setGraphScore($scope.dados.scores);
-        }
-
-        // Classes
-        $scope.dados.classes = [];
-        for (var a in data.tipos) {
-
-            var b = {};
-            b = data.tipos[a];
-
-            var win = parseInt(data.tipos[a].win);
-            var draw = parseInt(data.tipos[a].draw);
-            var loss = parseInt(data.tipos[a].loss);
-
-            var total = win + draw + loss;
-
-            var winPercentage = (win / total) * 100;
-            var lossPercentage = (loss / total) * 100;
-
-            b.classe = a;
-            b.total = total;
-            b.winPercentage = winPercentage;
-            b.lossPercentage = lossPercentage;
-
-            $scope.dados.classes.push(b);
-        }
-        console.dir($scope.dados.classes);
-
-        if (!$scope.$$phase) {
-            $scope.$apply();
-        }
+        $scope.dados.porMapa = porMapa;
 
     	
     }, 1000);
